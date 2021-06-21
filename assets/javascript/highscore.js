a6f027cc1cd7de83a5eb8794514959e570d4dbb9
Spencer Badger <spbadger@hotmail.com>
2021-06-21 11:59:01 +1000

    Updating files

--- assets/javascript/highscore.js
+++ assets/javascript/highscore.js
@@ -14,24 +14,16 @@ function clearHighScore() {
 
 displayScore();
 
-
 function displayScore() {
 
     var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
     highscores.sort(function(a, b) {
-        return a - b;
+        return b.playerName - a.playerName;
     });
     highscores.forEach(function(score) {
         console.log(highscores);
 
-        let count = 1;
-
-        count + " " + score.name + " " + score.playerScores
-        count++
-
         for (i = 0; i <= highscores.length; i++) {
-            // console.dir(highscores[i].name);
-            // console.dir(highscores[i].playerName);
             var nameInfo = highscores[i].name;
             var scoreInfo = highscores[i].playerName;
             var liOne = document.createElement('li');
@@ -42,34 +34,8 @@ function displayScore() {
             var ulTwo = sTable;
             sTable.appendChild(liTwo);
             liTwo.innerHTML = scoreInfo;
-            // var liThree = document.createElement('li');
-            // var ulThree = Rank;
-            // Rank.appendChild(liThree);
-            // liThree.innerHTML = counter;
+
 
         }
     })
 }
-
-
-
-const hsCont = document.getElementById('hs-container');
-
-// window.onload = function() {
-
-//     if (highscores = JSON.parse(window.localStorage.getItem("highscores"))) {
-//         console.log('information present');
-//         hsContainer.classList.remove('hide');
-//         displayScore();
-//     } else {
-//         const Cont = document.getElementById('no-score');
-//         Cont.classList.remove('.hide');
-//         hsCont.classList.add('.hide');
-//     }
-// }
-
-
-
-// function highScore(){
-//     highScoreT.classList.add('hide');
-// }
